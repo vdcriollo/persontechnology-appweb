f57a0fa5fb634c33cd6fafcb187a9d9f3ce77df9
persontechnology <persontechnologys@gmail.com>
2025-07-26 16:01:46 -0500

    end finaly

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    
     // Artisan::call('cache:clear');
     // Artisan::call('config:clear');
     // Artisan::call('config:cache');
